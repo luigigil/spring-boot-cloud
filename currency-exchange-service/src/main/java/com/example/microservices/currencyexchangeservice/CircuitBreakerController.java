81c3410f3360757c4eeaef91d80b45049d98ffcd
Luigi Gil <luigi.gil@vortex.com.br>
2022-01-11 11:00:23 -0300

    Implementation of @CircuitBreaker, @RateLimiter and @Bulkhead annotations. Commented some code for testing purposes

--- currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -14,13 +17,17 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    // @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    // @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    // @RateLimiter(name = "default")
+    @Bulkhead(name = "sample-api")
     public String sampleApi() {
         logger.info("Sample Api call received");
-        ResponseEntity<String> forEntity = new RestTemplate()
-                .getForEntity("http://localhost:8080/some-dummy-url", String.class); // Simulates down endpoint
+        // ResponseEntity<String> forEntity = new RestTemplate()
+        //         .getForEntity("http://localhost:8080/some-dummy-url", String.class); // Simulates down endpoint
 
-        return forEntity.getBody();
+        // return forEntity.getBody();
+        return "Sample api";
     }
 
     public String hardcodedResponse(Exception ex) {
